89acc79123d76de2123b28addf54ede694f6dc57
Gregory06 <gregpost@list.ru>
2017-08-04 20:21:43 +0300

    Add files via upload

--- engine.h
+++ engine.h
@@ -9,7 +9,7 @@
 
 sf::RenderWindow window(sf::VideoMode(1920, 1080), "A Swagabitch game");
 
-std::string Images_Dir("/home/fedya/Изображения/Game images/");
+std::string Images_Dir("/Users/gregpost/CLionProjects/Копия TOPPRYGA/");
 
 void Show_Kirill() {
     sf::Texture Kirill;
@@ -26,6 +26,8 @@ void Show_Kirill() {
 sf::Vector2i Start;
 sf::Vector2i Now;
 sf::Vector2i Finish;
+sf::Vector2u size = window.getSize();
+const unsigned int width = size.x / 120, height = size.y / 120;
 
 
 bool FirstPress = true, FirstPress1 = true, FirstPress2 = true, StartSet = false;
@@ -54,27 +56,20 @@ public:
     sf::Sprite block_spr;
 
     Block() {
-        block_tex.loadFromFile(Images_Dir + "wall.jpg");
+        block_tex.loadFromFile(Images_Dir + "wall.png");
         sf::Sprite sprite(block_tex);
         block_spr = sprite;
-        int width = block_tex.getSize().x;
-        int height = block_tex.getSize().y;
-        block_spr.setOrigin(width / 2, height / 2);
+        int Block_width = block_tex.getSize().x;
+        int Block_height = block_tex.getSize().y;
+        block_spr.setOrigin(Block_width / 2, Block_height / 2);
     }
 
-    void Create(class Point blocks[][16]) {
+    void Create(class Point blocks[][width]) {
 
         float max = 1000000;
         if (FirstPress1) {
-            for (int i = 0; i < 9; i++)
-                for (int j = 0; j < 16; j++) {
-                    //Убрать это
-                    if (Distance(sf::Mouse::getPosition(window), blocks[i][j].point_pos) < max) {
-                        max = Distance(sf::Mouse::getPosition(window), blocks[i][j].point_pos);
-                        sf::Vector2i vector(i, j);
-                        FocusOn = vector;
-                    }
-                }
+            sf::Vector2i vector(sf::Mouse::getPosition(window).y / 120, sf::Mouse::getPosition(window).x / 120); // переделал
+            FocusOn = vector;
             if (blocks[FocusOn.x][FocusOn.y].passable) {
                 blocks[FocusOn.x][FocusOn.y].passable = false;
                 block_spr.setPosition(blocks[FocusOn.x][FocusOn.y].point_pos.x,
@@ -85,16 +80,14 @@ public:
         FirstPress1 = false;
     }
 
-    void ShowBlocks(class Point blocks[][16]) {
+    void ShowBlocks(class Point blocks[][width]) {
         int i, j;
-        for (i = 0; i < 9; i++)
-            for (j = 0; j < 16; j++)
+        for (i = 0; i < height; i++)
+            for (j = 0; j < width; j++)
                 if (blocks[i][j].passable == false) {
                     block_spr.setPosition(blocks[i][j].point_pos.x, blocks[i][j].point_pos.y);
                     window.draw(block_spr);
                 }
-        //sf::Vector2i vector(0, 0);
-        //FocusOn = vector;
     }
 };
 
@@ -109,7 +102,7 @@ public:
     sf::Vector2i Start, Finish;
 
     Traveler() {
-        travel_tex.loadFromFile(Images_Dir + "path.jpg");
+        travel_tex.loadFromFile(Images_Dir + "path.png");
         sf::Sprite sprite(travel_tex);
         travel_spr = sprite;
         int width = travel_tex.getSize().x;
@@ -117,20 +110,18 @@ public:
         travel_spr.setOrigin(width / 2, height / 2);
     }
 
-    void create(class Point blocks[][16]) {
+    void create(class Point blocks[][width]) {
         int i, j;
         float min = 1000000;
         if (FirstPress2) {
-            for (i = 0; i < 9; i++)
-                for (j = 0; j < 16; j++) {
-                    if (!StartSet)
-                        blocks[i][j].partOfJorney = false;
-                    if (Distance(sf::Mouse::getPosition(window), blocks[i][j].point_pos) < min) {//Это всё тоже
-                        min = Distance(sf::Mouse::getPosition(window), blocks[i][j].point_pos);
-                        sf::Vector2i vector(i, j);
-                        FocusOn = vector;
-                    }
-                }
+            for (i = 0; i < height; i++)
+                for (j = 0; j < width; j++)
+                    blocks[i][j].partOfJorney = false;
+
+            sf::Vector2i vector(sf::Mouse::getPosition(window).y / 120,
+                                sf::Mouse::getPosition(window).x / 120);
+            FocusOn = vector;
+
             if ((blocks[FocusOn.x][FocusOn.y].passable) && !StartSet) {
                 travel_spr.setPosition(blocks[FocusOn.x][FocusOn.y].point_pos.x,
                                        blocks[FocusOn.x][FocusOn.y].point_pos.y);
@@ -139,18 +130,20 @@ public:
                 blocks[FocusOn.x][FocusOn.y].wave = 0;
             } else if (StartSet && (blocks[FocusOn.x][FocusOn.y].passable)) {
                 StartSet = false;
-                sf::Vector2i vector(FocusOn.x, FocusOn.y);
-                Finish = vector;
+                Finish = FocusOn;
             }
 
         }
+        // printf("Start: %d %d\nFinish: %d %d\n", Start.x, Start.y, Finish.x, Finish.y);
         FirstPress2 = false;
     }
 
-    void Wave(/*class*/ Point blocks[][16]) {//Зачем в таких местах писать class? Так делают только в структурах, когда не знают про typedef
-        for (int k = 0; k < 400; k++) {
-            for (int i = 0; i < 9; i++) {
-                for (int j = 0; j < 16; j++) {
+    void Wave(/*class*/ Point blocks[][width]) {//Зачем в таких местах писать class? Так делают только в структурах, когда не знают про typedef
+        int OgrSv = 0;
+        do {
+            OgrSv++;
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
                     if ((blocks[i][j].wave >= 0) && (blocks[i][j].passable)) {//Попроще
                         if ((blocks[i - 1][j].wave < 0) && blocks[i - 1][j].passable)
                             blocks[i - 1][j].wave = blocks[i][j].wave + 1;
@@ -160,75 +153,48 @@ public:
                             blocks[i + 1][j].wave = blocks[i][j].wave + 1;
                         if ((blocks[i][j + 1].wave < 0) && blocks[i][j + 1].passable)
                             blocks[i][j + 1].wave = blocks[i][j].wave + 1;
-//                      if ((blocks[i - 1][j - 1].wave < 0) && blocks[i - 1][j - 1].passable)
-//                          blocks[i - 1][j - 1].wave = blocks[i][j].wave + 1;
-//                      if ((blocks[i - 1][j + 1].wave < 0) && blocks[i - 1][j + 1].passable)
-//                          blocks[i - 1][j + 1].wave = blocks[i][j].wave + 1;
-//                      if ((blocks[i + 1][j - 1].wave < 0) && blocks[i + 1][j - 1].passable)
-//                          blocks[i + 1][j - 1].wave = blocks[i][j].wave + 1;
-//                      if ((blocks[i + 1][j + 1].wave < 0) && blocks[i + 1][j + 1].passable)
-//                          blocks[i + 1][j + 1].wave = blocks[i][j].wave + 1;
                     }
                 }
             }
-        }
+            if (OgrSv > height * width / 2) break;
+        } while (blocks[Finish.x][Finish.y].wave == -1);
     }
 
-    void GoBack(class Point blocks[][16]) {
-        int k;
-        for (k = 0; k < 1000; k++) {
+    void GoBack(class Point blocks[][width]) {
+        blocks[Finish.x][Finish.y].partOfJorney = true;
+        int OgrSv = 0;
+        do {
+            OgrSv++;
             if (blocks[Finish.x][Finish.y - 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
                 Finish.y = Finish.y - 1;
-                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
                 blocks[Finish.x][Finish.y].partOfJorney = true;
             } else if (blocks[Finish.x][Finish.y + 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
                 Finish.y = Finish.y + 1;
-                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
                 blocks[Finish.x][Finish.y].partOfJorney = true;
             } else if (blocks[Finish.x - 1][Finish.y].wave - blocks[Finish.x][Finish.y].wave == -1) {
                 Finish.x = Finish.x - 1;
-                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
                 blocks[Finish.x][Finish.y].partOfJorney = true;
             } else if (blocks[Finish.x + 1][Finish.y].wave - blocks[Finish.x][Finish.y].wave == -1) {
                 Finish.x = Finish.x + 1;
-                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
                 blocks[Finish.x][Finish.y].partOfJorney = true;
-            } //else
-//            if (blocks[Finish.x - 1][Finish.y - 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
-//                Finish.y = Finish.y - 1;
-//                Finish.x = Finish.x - 1;
-//                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
-//                blocks[Finish.x][Finish.y].partOfJorney = true;
-//            } else
-//            if (blocks[Finish.x + 1][Finish.y - 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
-//                Finish.y = Finish.y - 1;
-//                Finish.x = Finish.x + 1;
-//                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
-//                blocks[Finish.x][Finish.y].partOfJorney = true;
-//            } else
-//            if (blocks[Finish.x - 1][Finish.y + 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
-//                Finish.y = Finish.y + 1;
-//                Finish.x = Finish.x - 1;
-//                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
-//                blocks[Finish.x][Finish.y].partOfJorney = true;
-//            } else
-//            if (blocks[Finish.x + 1][Finish.y + 1].wave - blocks[Finish.x][Finish.y].wave == -1) {
-//                Finish.y = Finish.y + 1;
-//                Finish.x = Finish.x + 1;
-//                travel_spr.setPosition(blocks[Finish.x][Finish.y].point_pos.x, blocks[Finish.x][Finish.y].point_pos.y);
-//                blocks[Finish.x][Finish.y].partOfJorney = true;
-//            }
-        }
+            }
+            if (OgrSv > height * width / 2) break;
+        } while (Finish != Start);
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                blocks[i][j].wave = -1;
     }
 
-    void ShowJorney(class Point blocks[][16]) {
+    void ShowJorney(class Point blocks[][width]) {
         int i, j;
-        for (i = 0; i < 9; i++)
-            for (j = 0; j < 16; j++)
+        for (i = 0; i < height; i++)
+            for (j = 0; j < width; j++)
                 if (blocks[i][j].partOfJorney) {
                     travel_spr.setPosition(blocks[i][j].point_pos.x, blocks[i][j].point_pos.y);
                     window.draw(travel_spr);
                 }
+        travel_spr.setPosition(blocks[Start.x][Start.y].point_pos.x, blocks[Start.x][Start.y].point_pos.y);
+        window.draw(travel_spr);
     }
 };
 
@@ -270,7 +236,7 @@ public:
 
     sf::Texture background_image;
     sf::Texture Allot_t;
-    Point blocks[9][16];
+    Point blocks[height][width];
     Block AlgoritmLi;
 
     std::string bg_path = Images_Dir + "bg.jpg";
@@ -278,28 +244,27 @@ public:
     void run();
 
     Engine(List<Physical_Body> &objects) : objects(objects) {
-
         blocks[0][0].point_pos.x = 60;
         blocks[0][0].point_pos.y = 60;
-        for (int i = 0, j = 0; i < 9; i++) {
+        for (int i = 0, j = 0; i < height; i++) {
             if ((i != 0) || (j != 0)) {
                 blocks[i][0].point_pos.y = blocks[i - 1][0].point_pos.y + 120;
                 blocks[i][0].point_pos.x = blocks[i - 1][0].point_pos.x;
             }
-            for (j = 1; j < 16; j++) {
+            for (j = 1; j < width; j++) {
                 if ((i != 0) || (j != 0)) {
                     blocks[i][j].point_pos.x = blocks[i][j - 1].point_pos.x + 120;
                     blocks[i][j].point_pos.y = blocks[i][j - 1].point_pos.y;
                 }
             }
         }
-        for (int i = 0; i < 9; i++)
-            for (int j = 0; j < 16; j++)
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
                 if (((i < 1) || (i > 7)) || ((j < 1) || (j > 14)))
                     blocks[i][j].passable = false;
 
         background_image.loadFromFile(bg_path);
-        Allot_t.loadFromFile(Images_Dir + "texture.jpg");
+        Allot_t.loadFromFile(Images_Dir + "texture.png");
 
     };
 
@@ -330,6 +295,7 @@ void Engine::run() {
     sf::Clock clock;
     ////
     while (window.isOpen()) {
+        window.setSize(sf::Vector2u(width * 120, height * 120));
         ////
         clock.restart();
         ////
@@ -349,7 +315,9 @@ void Engine::run() {
         if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
             Allot(Allot_s);
             bob.create(blocks);
-            if (!StartSet) {
+            if (StartSet) {
+                bob.create(blocks);
+            } else {
                 bob.Wave(blocks);
                 bob.GoBack(blocks);
             }
